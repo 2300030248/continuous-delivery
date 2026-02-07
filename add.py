f3f8baf92c883287d60c85e42d2c99d6a00ca560
2300030248 <harithaharithavelagapudi@gmail.com>
2026-02-07 10:51:33 +0530

    first comment

---
+++ add.py
@@ -0,0 +1,5 @@
+#This prg add two numbers
+num1 = 1. 5
+num2 = 6. 3
+sum = num1 + num2
+print('The sum 0f(0) and (!)[1[}[D[D{1}[D[D[[D[D[D[D[}[D[{[C[C[C[C[C[C[C[[C[C[C[C[D[D[C[[C[C[C[C is {2}'.format(num1, num2, sum)
